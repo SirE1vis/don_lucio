6e292c8b8b25286ae99ea51b94be96743107d1a7
Elvis-MV <macavilcavilla06@gmail.com>
2025-06-30 19:22:28 -0500

    configuración de SEO y dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://SirE1vis.github.io',
+  site: 'https://donsasaa.com', 
   base: '/',
   
   integrations: [
     tailwind(),
-    preact()
+    preact(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
   ],
 
   output: 'static',
